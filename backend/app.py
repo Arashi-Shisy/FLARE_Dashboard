9448a54017e3288e451648b9d2908eaaa7e001f6
arabcoshi <arabcoshi.letsmusic@gmail.con>
2025-10-19 16:38:48 +0900

    ファイル上限

--- backend/app.py
+++ backend/app.py
@@ -23,7 +23,7 @@ def create_app():
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SECRET_KEY"] = SECRET_KEY
     app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(days=31)
-    app.config["MAX_CONTENT_LENGTH"] = 10 * 1024 * 1024
+    app.config["MAX_CONTENT_LENGTH"] = 40 * 3840 * 3840
     app.config["UPLOAD_FOLDER"] = UPLOAD_DIR
 
     CORS(app, supports_credentials=True)
